f56d28991c6c986159a8653e6451a7666a2a43c7
Abrahamjondev <ibrohimjon0425@gmail.com>
2025-12-26 21:58:29 +0500

    feat: develop auther router ejs page

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ console.log("Web serverni boshlash");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
 
-//1 KIRISH CODES
+let user;
+fs.readFile("database/users.json", "utf-8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
+
+//1 KIRISH CODES. kirish malumotlarga bogliq bolgan kodlar yoziladi
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -17,8 +27,11 @@ app.set("view engine", "ejs");
 //4 ROUTING CODES
 app.post("/create-item", (req, res) => {
   console.log(req.body);
-  res.json ({ test: "success" });
+  res.json({ test: "success" });
   // Ma'lumotlarni qabul qilish
+});
+app.get("/auther", (req, res) => {
+  res.render("auther", { user: user });
 });
 app.get("/", function (req, res) {
   res.render("harid");
