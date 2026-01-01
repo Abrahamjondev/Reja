5e8f12e70a0a0e2f7d6b0a50a839c658838bb4e6
Abrahamjondev <ibrohimjon0425@gmail.com>
2026-01-01 15:19:09 +0500

    Feat: crud operatsiyalari

--- app.js
+++ app.js
@@ -43,70 +43,139 @@
 
 // module.exports = app;
 
-console.log("Web Serverni Boshlash");
-const { log } = require("console");
+
+
+
+
+//
+//
+
+//
+// console.log("Web Serverni Boshlash");
+// const { log } = require("console");
+// const express = require("express");
+// const app = express(); // expressning objecti olindi
+// const fs = require("fs");
+
+// //MongoDB
+
+// const db = require("./server").db();
+
+// let user;
+// fs.readFile("database/user.json", "utf-8", (err, data) => {
+//   if (err) {
+//     console.log("ERROR", err);
+//   } else {
+//     user = JSON.parse(data);
+//   }
+// });
+
+// // 1: Kirish code                       // Bu qismdagi use bu => DP => bog'lab beryabdi
+// app.use(express.static("public")); // Clientlar uchun public folderni ochib berdik
+// app.use(express.json()); // Kirib kelayotgan json formatni objectga uguradi
+// app.use(express.urlencoded({ extended: true })); // Trad. HTML formdan post qilingan narsalarni togri qabul qiladi
+
+// // 2: Session code
+
+// // BSSR backend side site rendering || backendda fronted qurish
+// // 3: Views code
+// app.set("views", "views"); // Folderni korsatdik => 1 o'zgarmaydi 2 folder nomi bilan mos bolishi kerak
+// app.set("view engine", "ejs"); // views folder ichidan o'qiydi EJS bilan [biz signal berdik shu engine`da bolaman]
+
+// // 4: Routing code      -       qaymoq joyi
+// // hamma / ga kelganlarga pastdagi qator chiqadi
+
+// app.post("/create-item", (req, res) => {
+//   console.log("user entered /create-item");
+//   console.log(req.body);
+//   const new_reja = req.body;
+//   db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
+//     if (err) {
+//       console.log(err);
+//       res.end("something went wrong");
+//     } else {
+//       res.end("succesfully added");
+//     }
+//   });
+// });
+// app.get("/", function (req, res) {
+//   console.log("user entered /");
+//   db.collection("plans")
+//     .find()
+//     .toArray((err, data) => {
+//       if (err) {
+//         console.log(err);
+//         res.end("something went wrong");
+//       } else {
+//         console.log(data);
+//         res.render("reja", { items: data });
+//       }
+//     });
+// });
+// // app.get("/author", (req, res) => {
+// //   res.render("author", { user: user });
+// // });
+
+// module.exports = app;
+///
+//
+
+//
+
+//
+console.log("Web serverni boshlash");
 const express = require("express");
-const app = express(); // expressning objecti olindi
-const fs = require("fs");
+const app = express();
+// const fs = require("fs");
 
-//MongoDB
 
+//Calling MongoDB
 const db = require("./server").db();
 
-let user;
-fs.readFile("database/user.json", "utf-8", (err, data) => {
-  if (err) {
-    console.log("ERROR", err);
-  } else {
-    user = JSON.parse(data);
-  }
-});
 
-// 1: Kirish code                       // Bu qismdagi use bu => DP => bog'lab beryabdi
-app.use(express.static("public")); // Clientlar uchun public folderni ochib berdik
-app.use(express.json()); // Kirib kelayotgan json formatni objectga uguradi
-app.use(express.urlencoded({ extended: true })); // Trad. HTML formdan post qilingan narsalarni togri qabul qiladi
+//1 KIRISH CODE
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// 2: Session code
+//2 session CODES
 
-// BSSR backend side site rendering || backendda fronted qurish
-// 3: Views code
-app.set("views", "views"); // Folderni korsatdik => 1 o'zgarmaydi 2 folder nomi bilan mos bolishi kerak
-app.set("view engine", "ejs"); // views folder ichidan o'qiydi EJS bilan [biz signal berdik shu engine`da bolaman]
+//3 VIEWS CODES
+app.set("views", "views");
+app.set("view engine", "ejs");
 
-// 4: Routing code      -       qaymoq joyi
-// hamma / ga kelganlarga pastdagi qator chiqadi
+
+//4 ROUTING CODES
 app.get("/", function (req, res) {
-  console.log("user entered /");
-  db.collection("plans")
-    .find()
-    .toArray((err, data) => {
-      if (err) {
-        console.log(err);
-        res.end("something went wrong");
-      } else {
-        console.log(data);
-        res.render("reja", { items: data });
-      }
-    });
-});
-
-app.post("/create-item", (req, res) => {
-  console.log("user entered /create-item");
-  console.log(req.body);
-  const new_reja = req.body;
-  db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
-    if (err) {
+  console.log("user entered to here /")
+  db.collection("plans").find().toArray((err, data)=>{
+    if(err) {
       console.log(err);
-      res.end("something went wrong");
-    } else {
-      res.end("succesfully added");
+      res.end("something went wrong!");
+    }else{
+      console.log(data);
+      res.render("reja", {items: data})
     }
   });
 });
 
-app.get("/author", (req, res) => {
-  res.render("author", { user: user });
-});
+app.post("/create-item", (req, res)=>{
+  console.log("user entered here to /create-item")
+  console.log(req.body);
+  const new_reja = req.body.reja;
+  db.collection("plans").insertOne({reja: new_reja}, (err, data)=>{
+    if(err){
+      console.log(err);
+      res.end('Something went worng!');
+    }else{
+      res.end("successfully added");
+    }
+  })
+})
 
 module.exports = app;
+
+
+// app.get("/author", (req, res) => {
+//   res.render("author.ejs", { user: user });
+// });
--- train.js
+++ train.js
@@ -1,30 +1,41 @@
-//MIT TASK
-function countLetter(letter, word) {
-  let count = 0;
-
-  for (let i = 0; i < word.length; i++) {
-    if (word[i] === letter) {
-      count++;
+// //MIT TASK
+//TASK B
+function countDigit(a){
+    let count = 0;
+    for(let char of a){
+        if(char >= '0' && char <= '9'){
+            count ++;
+        }
     }
-  }
-
-  return count;
+    return count;
 }
-console.log("=========");
-console.log(countLetter("e", "engineer"));
-console.log("=========");
+console.log(countDigit("ad2a54y79wet0sfgb9"));
+// function countLetter(letter, word) {
+//   let count = 0;
 
-// NodeJS event loop va Callback functionlarni o'rganamiz
-//CALLBACK FUNCTIONS
-console.log("Jack Ma naslahatlari");
-const list = [
-  "Yaxshi talaba boling", //0-20 =>index [0]
-  "Togri boshliq tanlang va koproq xato qiling", //20-30. =>index [1]
-  "Uzingizga ishlashingizni boshlang", //30-40. =>index [2]
-  "Siz kuchli bolgan narsalarni qiling", //40-50 =>index [3]
-  "Yoshlarga investitisiya qiling", //50-60 =>index [4]
-  "Endi dam oling, foyasi yoq", //60++. =>index [5]
-];
+//   for (let i = 0; i < word.length; i++) {
+//     if (word[i] === letter) {
+//       count++;
+//     }
+//   }
+
+//   return count;
+// }
+// console.log("=========");
+// console.log(countLetter("e", "engineer"));
+// console.log("=========");
+
+// // NodeJS event loop va Callback functionlarni o'rganamiz
+// //CALLBACK FUNCTIONS
+// console.log("Jack Ma naslahatlari");
+// const list = [
+//   "Yaxshi talaba boling", //0-20 =>index [0]
+//   "Togri boshliq tanlang va koproq xato qiling", //20-30. =>index [1]
+//   "Uzingizga ishlashingizni boshlang", //30-40. =>index [2]
+//   "Siz kuchli bolgan narsalarni qiling", //40-50 =>index [3]
+//   "Yoshlarga investitisiya qiling", //50-60 =>index [4]
+//   "Endi dam oling, foyasi yoq", //60++. =>index [5]
+// ];
 
 // function maslahatBering(a, callback) {
 //   if (typeof a !== "number") callback("please insert a number", null);
@@ -51,24 +62,24 @@ const list = [
 
 // LESSON 22
 //Asynchronous functionlarni qo'llash
-async function maslahatBering(a) {
-  if (typeof a !== "number") throw new Error("please insert a number");
-  else if (a <= 20) return list[0];
-  else if (a > 20 && a <= 30) return list[1];
-  else if (a > 30 && a <= 40) return list[2];
-  else if (a > 40 && a <= 50) return list[3];
-  else if (a > 50 && a >= 60) return list[4];
-  else {
-    return new Promise((resolve, reject) => {
-      setInterval(() => {
-        resolve(list[5]);
-      }, 1000);
-    });
-    // setTimeout(function () {
-    //   return list[5];
-    // }, 5000);
-  }
-}
+// async function maslahatBering(a) {
+//   if (typeof a !== "number") throw new Error("please insert a number");
+//   else if (a <= 20) return list[0];
+//   else if (a > 20 && a <= 30) return list[1];
+//   else if (a > 30 && a <= 40) return list[2];
+//   else if (a > 40 && a <= 50) return list[3];
+//   else if (a > 50 && a >= 60) return list[4];
+//   else {
+//     return new Promise((resolve, reject) => {
+//       setInterval(() => {
+//         resolve(list[5]);
+//       }, 1000);
+//     });
+//     // setTimeout(function () {
+//     //   return list[5];
+//     // }, 5000);
+//   }
+// }
 ////CALL VIA //then, catch
 // console.log("passed here 0 ");
 // maslahatBering(75)
@@ -80,16 +91,16 @@ async function maslahatBering(a) {
 //   });
 // console.log("passed here 1 ");
 
-//CALL VIA/asyn/AWAIT
-async function run() {
-  let javob = await maslahatBering(75);
-  console.log(javob);
-  javob = await maslahatBering(70);
-  console.log(javob);
-  javob = await maslahatBering(41);
-  console.log(javob);
-}
-run();
+// //CALL VIA/asyn/AWAIT
+// async function run() {
+//   let javob = await maslahatBering(75);
+//   console.log(javob);
+//   javob = await maslahatBering(70);
+//   console.log(javob);
+//   javob = await maslahatBering(41);
+//   console.log(javob);
+// }
+// run();
 
 // function maslahatBering(a, callback) {
 //   if (typeof a !== "number") callback("please insert a number", null);
