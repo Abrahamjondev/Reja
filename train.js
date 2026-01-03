166547a79d7f8920942af48229f842d572454d37
Abrahamjondev <ibrohimjon0425@gmail.com>
2026-01-03 07:34:44 +0500

    fix: midified train.js

--- train.js
+++ train.js
@@ -1,5 +1,12 @@
 // //MIT TASK
 //TASK C
+//MITASK-C
+
+// Shunday class tuzing tuzing nomi Shop, va uni constructoriga 3 hil mahsulot pass bolsin, hamda classning 3ta methodi bolsin, biri qoldiq, biri sotish va biri qabul. Har bir method ishga tushgan vaqt ham log qilinsin.
+// MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud! shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq() return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!
+
+// @MITASK
+//
 const moment = require("moment");
 const date = moment().format("h:mm:ss a");
 class Shop {
@@ -24,6 +31,7 @@ const shop = new Shop(4, 5, 2);
 console.log(shop.sotish("non", 3));
 console.log(shop.qabul("cola", 4));
 console.log(shop.qoldiq());
+
 //TASK B
 // function countDigit(a){
 //     let count = 0;
