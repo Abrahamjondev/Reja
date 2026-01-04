8214f8d2cea3b439a69b2b7db3a4ac7ee5342fe2
Abrahamjondev <ibrohimjon0425@gmail.com>
2026-01-04 16:49:22 +0500

    feat: MIT TASK C IS DONE

--- train.js
+++ train.js
@@ -1,36 +1,55 @@
 // //MIT TASK
-//TASK C
-//MITASK-C
 
-// Shunday class tuzing tuzing nomi Shop, va uni constructoriga 3 hil mahsulot pass bolsin, hamda classning 3ta methodi bolsin, biri qoldiq, biri sotish va biri qabul. Har bir method ishga tushgan vaqt ham log qilinsin.
-// MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud! shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq() return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!
+// TASK D:
+// Shunday function tuzing, u 2ta string parametr ega bolsin, hamda agar har ikkala string bir hil harflardan iborat bolsa true aks holda false qaytarsin
+// MASALAN checkContent ("mitgroup",
+// "gmtiprou") return qiladi true;
 
-// @MITASK
-//
-const moment = require("moment");
-const date = moment().format("h:mm:ss a");
-class Shop {
-  constructor(non, lagmon, cola) {
-    this.non = non;
-    this.lagmon = lagmon;
-    this.cola = cola;
-  }
+//ANSWER
+function checkContent(str1, str2) {
+  if (str1.length !== str2.length) return false;
 
-  qoldiq() {
-    return `Hozir ${date} da: bizda ${this.non}ta non, ${this.lagmon}ta lagmon va ${this.cola}ta cola qoldi`;
-  }
-  sotish(mahsulot, soni) {
-    return (this[mahsulot] -= soni);
-  }
-  qabul(mahsulot, soni) {
-    return (this[mahsulot] += soni);
-  }
+  const string1 = str1.split('').sort().join('');
+  const string2 = str2.split('').sort().join('');
+
+  return string1 === string2
 }
 
-const shop = new Shop(4, 5, 2);
-console.log(shop.sotish("non", 3));
-console.log(shop.qabul("cola", 4));
-console.log(shop.qoldiq());
+console.log(checkContent("mitgroup", "gmtiprou")); 
+console.log(checkContent("nodemon", "express"));      
+
+// //TASK C
+// //MITASK-C
+
+ //Shunday class tuzing tuzing nomi Shop, va uni constructoriga 3 hil mahsulot pass bolsin, hamda classning 3ta methodi bolsin, biri qoldiq, biri sotish va biri qabul. Har bir method ishga tushgan vaqt ham log qilinsin.
+ //MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud! shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq() return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!
+
+// // @MITASK
+// //
+// const moment = require("moment");
+// const date = moment().format("h:mm:ss a");
+// class Shop {
+//   constructor(non, lagmon, cola) {
+//     this.non = non;
+//     this.lagmon = lagmon;
+//     this.cola = cola;
+//   }
+
+//   qoldiq() {
+//     return `Hozir ${date} da: bizda ${this.non}ta non, ${this.lagmon}ta lagmon va ${this.cola}ta cola qoldi`;
+//   }
+//   sotish(mahsulot, soni) {
+//     return (this[mahsulot] -= soni);
+//   }
+//   qabul(mahsulot, soni) {
+//     return (this[mahsulot] += soni);
+//   }
+// }
+
+// const shop = new Shop(4, 5, 2);
+// console.log(shop.sotish("non", 3));
+// console.log(shop.qabul("cola", 4));
+// console.log(shop.qoldiq());
 
 //TASK B
 // function countDigit(a){
